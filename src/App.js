6f423877dcbd98c28898219fd9a8517f7cb92bee
patrik <patrik.olsson@axiomatics.com>
2019-02-07 09:24:24 +0100

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Switch from '@material-ui/core/Switch';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      bountyRuneSpawning: true,
+    };
+  }
+
+  handleToggleSwitch() {
+    this.setState(state => ({
+      bountyRuneSpawning: !state.bountyRuneSpawning,
+    }));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className="App-header" />
+        <span>Bounty rune will spawn</span>
+        <Switch
+          checked={this.state.bountyRuneSpawning}
+          onChange={() => this.handleToggleSwitch()}
+          color="primary"
+        />
       </div>
     );
   }

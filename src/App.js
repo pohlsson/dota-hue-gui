8ea3fe663c9b0d7fe5121f66649dc2ce0330573c
patrik <patrik.olsson@axiomatics.com>
2019-02-07 16:56:13 +0100

    Add select for colors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React, {Component} from 'react';
 import Switch from '@material-ui/core/Switch';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+
 import './App.css';
+
+const colors = {
+  red: {
+    hue: 0,
+    sat: 100,
+    bri: 66,
+  },
+  blue: {
+    hue: 234,
+    sat: 100,
+    bri: 66,
+  }
+};
 
 class App extends Component {
 
@@ -8,6 +24,7 @@ class App extends Component {
     super(props);
     this.state = {
       bountyRuneSpawning: true,
+      color: colors.red,
     };
   }
 
@@ -15,6 +32,10 @@ class App extends Component {
     this.setState(state => ({
       bountyRuneSpawning: !state.bountyRuneSpawning,
     }));
+  }
+
+  handleSelectColor(color) {
+    this.setState({color: colors[color]});
   }
 
   render() {
@@ -27,6 +48,18 @@ class App extends Component {
           onChange={() => this.handleToggleSwitch()}
           color="primary"
         />
+        <Select
+          value={this.state.color}
+          onChange={input => this.handleSelectColor(input.target.value)}
+          inputProps={{
+            name: Object.keys(this.state.color)[0],
+          }}
+        >
+          {console.log(this.state.color)}
+          {Object.keys(colors).map(color => (
+            <MenuItem value={color}>{color}</MenuItem>
+          ))}
+        </Select>
       </div>
     );
   }
